3b4b50bf50b78297879130cc88198e85c134157c
Nabil Salah <nabil.salah203@gmail.com>
2022-05-14 23:30:05 +0200

    Prototype for customer

--- Market/Program.cs
+++ Market/Program.cs
@@ -2,6 +2,54 @@
 
 namespace Market
 {
+    public class customer
+    {
+        protected string Name;
+        protected int price;
+        protected int[] cart= new int[50];
+        private int i;
+        public void addTocart(int item)
+        {
+            cart[i] = item;
+        }
+        public customer()
+        {
+            i = 0;
+            Name = "Default Name";
+            price = 0;
+        }
+        public customer(string Name,int price)
+        {
+            this.Name = Name;
+            this.price = price;
+            i = 0;
+        }
+    }
+    public class visaCustomer : customer
+    {
+        public double discount;
+        public visaCustomer(string Name,int price)
+        {
+            this.Name = Name;
+            this.price = price;
+        }
+        public double totalPaid()
+        {
+            return (double)price*discount;
+        } 
+    }
+    public class cashCustomer : customer
+    {
+        public cashCustomer(string Name, int price)
+        {
+            this.Name = Name;
+            this.price = price;
+        }
+        public int totalPaid()
+        {
+            return price;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
